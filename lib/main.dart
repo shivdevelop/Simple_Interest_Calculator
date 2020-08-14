cf27472b6f783c75ec9881d39108efae3804d6fc
Celestial Programmer <ss.3gupta@gmail.com>
2020-08-14 19:40:21 +0530

    Making UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main(){
-  runApp(MaterialApp(title: "Simple Interest Calculator",
-  home: SIForm(),));
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Simple Interest Calculator",
+    home: SIForm(),
+    theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent),
+  ));
 }
 
 class SIForm extends StatefulWidget {
@@ -12,11 +20,133 @@ class SIForm extends StatefulWidget {
   }
 }
 
-class _SIFormState extends State<Form> {
-  var _currencies = ''
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees', 'Dollors', 'Pounds', 'Yuan'];
+  final _minimumPadding = 5.0;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+    return Scaffold(
+//      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text("Simple Interest Calculator"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: [
+            getImageAsset(),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                style: textStyle,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Principal",
+                    labelStyle: textStyle,
+                    hintText: "Enter Principal Amount e.g. 12000",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                style: textStyle,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Rate of Interest",
+                    labelStyle: textStyle,
+                    hintText: "In Percent e.g. 10 is 10%  ",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: textStyle,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: "Time",
+                          labelStyle: textStyle,
+                          hintText: "Time in years",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      value: 'Rupees',
+                      onChanged: (String newValueSelected) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: RaisedButton(
+                          color: Theme.of(context).accentColor,
+                          textColor: Theme.of(context).primaryColorDark,
+                          child: Text(
+                            "CALCULATE",textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {})),
+                  Expanded(
+                      child: RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          textColor: Theme.of(context).primaryColorLight,
+                          child: Text(
+                            "RESET",textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {})),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Text(
+                'TODO Text',
+                style: textStyle,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage("images/money.png");
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
+    );
   }
 }
