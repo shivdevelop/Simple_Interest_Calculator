c942c6434072fba0e632f7976954474f51df5e04
Celestial Programmer <ss.3gupta@gmail.com>
2020-08-15 11:30:33 +0530

    Making UI

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,22 @@ class SIForm extends StatefulWidget {
 }
 
 class _SIFormState extends State<SIForm> {
+  var _formKey = GlobalKey<FormState>();
   var _currencies = ['Rupees', 'Dollors', 'Pounds', 'Yuan'];
   final _minimumPadding = 5.0;
+
+  var displayResult = "";
+  var _currentItemSelected = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,105 +46,157 @@ class _SIFormState extends State<SIForm> {
       appBar: AppBar(
         title: Text("Simple Interest Calculator"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(_minimumPadding * 2),
-        child: ListView(
-          children: [
-            getImageAsset(),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: TextField(
-                style: textStyle,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Principal",
-                    labelStyle: textStyle,
-                    hintText: "Enter Principal Amount e.g. 12000",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0))),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(_minimumPadding * 2),
+          child: ListView(
+            children: [
+              getImageAsset(),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextFormField(
+                  style: textStyle,
+                  keyboardType: TextInputType.number,
+                  controller: principalController,
+//                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter principal amount';
+                    }
+                    if (num.tryParse(value) == null) {
+                      return 'Please enter numerical value';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Principal",
+                      labelStyle: textStyle,
+                      hintText: "Enter Principal Amount e.g. 12000",
+                      errorStyle:
+                          TextStyle(color: Colors.yellowAccent, fontSize: 15.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: TextField(
-                style: textStyle,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Rate of Interest",
-                    labelStyle: textStyle,
-                    hintText: "In Percent e.g. 10 is 10%  ",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0))),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextFormField(
+                  style: textStyle,
+                  keyboardType: TextInputType.number,
+                  controller: roiController,
+//                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter Rate of Interest';
+                    }
+                    if (num.tryParse(value) == null) {
+                      return 'Please enter numerical value';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Rate of Interest",
+                      labelStyle: textStyle,
+                      hintText: "In Percent e.g. 10 is 10%  ",
+                      errorStyle:
+                          TextStyle(color: Colors.yellowAccent, fontSize: 15.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      style: textStyle,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          labelText: "Time",
-                          labelStyle: textStyle,
-                          hintText: "Time in years",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0))),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        style: textStyle,
+                        keyboardType: TextInputType.number,
+                        controller: timeController,
+//                        inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter Time';
+                          }
+                          if (num.tryParse(value) == null) {
+                            return 'Please enter numerical value';
+                          }
+                        },
+                        decoration: InputDecoration(
+                            labelText: "Time",
+                            labelStyle: textStyle,
+                            hintText: "Time in years",
+                            errorStyle: TextStyle(
+                                color: Colors.yellowAccent, fontSize: 15.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: _minimumPadding * 5,
-                  ),
-                  Expanded(
-                    child: DropdownButton<String>(
-                      items: _currencies.map((String value) {
-                        return DropdownMenuItem<String>(
-                            value: value, child: Text(value));
-                      }).toList(),
-                      value: 'Rupees',
-                      onChanged: (String newValueSelected) {},
+                    Container(
+                      width: _minimumPadding * 5,
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: DropdownButton<String>(
+                        items: _currencies.map((String value) {
+                          return DropdownMenuItem<String>(
+                              value: value, child: Text(value));
+                        }).toList(),
+                        value: _currentItemSelected,
+                        onChanged: (String newValueSelected) {
+                          _onDropDownItemSelected(newValueSelected);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: RaisedButton(
-                          color: Theme.of(context).accentColor,
-                          textColor: Theme.of(context).primaryColorDark,
-                          child: Text(
-                            "CALCULATE",textScaleFactor: 1.5,
-                          ),
-                          onPressed: () {})),
-                  Expanded(
-                      child: RaisedButton(
-                          color: Theme.of(context).primaryColorDark,
-                          textColor: Theme.of(context).primaryColorLight,
-                          child: Text(
-                            "RESET",textScaleFactor: 1.5,
-                          ),
-                          onPressed: () {})),
-                ],
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            textColor: Theme.of(context).primaryColorDark,
+                            child: Text(
+                              "CALCULATE",
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                if (_formKey.currentState.validate())
+                                  this.displayResult = _calculateSI();
+                              });
+                            })),
+                    Expanded(
+                        child: RaisedButton(
+                            color: Theme.of(context).primaryColorDark,
+                            textColor: Theme.of(context).primaryColorLight,
+                            child: Text(
+                              "RESET",
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _reset();
+                              });
+                            })),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: Text(
-                'TODO Text',
-                style: textStyle,
-              ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Text(
+                  this.displayResult,
+                  style: textStyle,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -148,5 +214,32 @@ class _SIFormState extends State<SIForm> {
       child: image,
       margin: EdgeInsets.all(_minimumPadding * 10),
     );
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentItemSelected = newValueSelected;
+    });
+  }
+
+  String _calculateSI() {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double time = double.parse(timeController.text);
+
+    double SI = (principal * roi * time) / 100;
+    double totalAmountPayable = principal + SI;
+
+    String result =
+        'After $time years, Your total amount payable is $totalAmountPayable $_currentItemSelected';
+    return result;
+  }
+
+  void _reset() {
+    principalController.text = "";
+    roiController.text = "";
+    timeController.text = "";
+    displayResult = "";
+    _currentItemSelected = _currencies[0];
   }
 }
